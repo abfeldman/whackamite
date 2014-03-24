074e33520792ae98bf4fb6ce186de8f42a0d5ce5
Alexander Feldman <alex@llama.gs>
2014-03-24 17:49:14 +0100

    Added Canny threshold detection.

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,57 @@
-#include <iostream>
-#include <cstdlib>
+#include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/highgui/highgui.hpp"
+
+#include <stdlib.h>
+#include <stdio.h>
+
+using namespace cv;
+
+Mat src, src_gray;
+Mat dst, detected_edges;
+
+int edgeThresh = 1;
+int lowThreshold;
+int const max_lowThreshold = 100;
+int ratio = 3;
+int kernel_size = 3;
+char window_name[] = "Edge Map";
+
+void cannyThreshold(int, void *)
+{
+    // Reduce noise with a kernel 3x3
+    blur(src_gray, detected_edges, Size(3, 3));
+
+    // Canny detector
+    Canny(detected_edges, detected_edges, lowThreshold, lowThreshold * ratio, kernel_size);
+
+    // Using Canny's output as a mask, we display our result
+    dst = Scalar::all(0);
+
+    src.copyTo(dst, detected_edges);
+    imshow(window_name, dst);
+}
 
 int main(int argc, char **argv)
 {
+    src = imread(argv[1]);
+    if (!src.data) {
+        return EXIT_FAILURE;
+    }
+
+    // Create a matrix of the same type and size as src (for dst).
+    dst.create(src.size(), src.type());
+
+    // Convert the image to grayscale.
+    cvtColor(src, src_gray, CV_BGR2GRAY);
+
+    namedWindow(window_name, CV_WINDOW_AUTOSIZE);
+    createTrackbar("Min Threshold:", window_name, &lowThreshold, max_lowThreshold, cannyThreshold);
+
+    // Show the image
+    cannyThreshold(0, 0);
+
+    // Wait until user exit program by pressing a key
+    waitKey(0);
+
     return EXIT_SUCCESS;
 }
